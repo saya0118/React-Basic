24a413c8cb60d1003460c18ec46c1a998b188d0f
saya0118 <chemi0802@gmail.com>
2021-11-14 22:02:05 -0800

    created edit function

--- src/Todo.js
+++ src/Todo.js
@@ -5,6 +5,8 @@ const Todo = () => {
     const [ searchValue, setSearchValue ] = useState("");
     const [ todos, setToDos] = useState([]);
 
+    const [ editIndex, setEditIndex] = useState(-1);
+
     const onChange = (e) => {
         setSearchValue(e.target.value)
     }
@@ -14,27 +16,46 @@ const Todo = () => {
         setSearchValue("");
     }
 
-    const onHandleDelete = index => {
-        // delete
-        const newToDos = [...todos]
-        newToDos.splice(index,1)
-        setToDos(newToDos)
+    // const onHandleDelete = index => {
+    //     // delete
+    //     const newToDos = [...todos]
+    //     newToDos.splice(index,1)
+    //     setToDos(newToDos)
+    // }
+
+    const onHandleDelete = (index) => setToDos(todos.filter((x,i)=> i !== index));
+
+    const onHandleEdit = (item, index) => {
+        // edit
+        setEditIndex(index);
+        setSearchValue(item);
     }
 
-    const onHandleEdit = () => {
-        // edit
-        
+    const onCompleteEdit = () => {
+        let newTodos = [...todos];
+    
+        newTodos[editIndex] = searchValue;
+    
+        setToDos(newTodos);
+        setEditIndex(-1);
+        setSearchValue("");
     }
 
     return (
         <div>
             <h3>To Do List</h3>
             <input value={searchValue} onChange={onChange}/>
-            <button onClick={onHandleAdd}>Add</button>
-            {
-                todos.map(todo => <li>{todo}<button onClick={onHandleDelete}>Delete</button>
-                    <button onClick={onHandleEdit}>Edit</button></li>)
+            {editIndex === -1 ?
+                <button onClick={onHandleAdd}>Add</button> :
+                <button onClick={onCompleteEdit}>OK</button>
             }
+            {todos.map((todo, i) => 
+                <li>
+                    {todo}
+                    <button onClick={() => onHandleEdit(todo, i)}>Edit</button>
+                    <button onClick={() => onHandleDelete(i)}>Delete</button>
+                </li>
+            )}
         </div>
     )
 }
